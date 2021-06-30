aec94f546926ec5705f5277754af78c9d84a6e6e
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-30 22:20:55 +0530

    ADDED: find Max of three intiger

--- src/com/bridgelabz/PrintMaxNumber.java
+++ src/com/bridgelabz/PrintMaxNumber.java
@@ -1,10 +1,23 @@
 package com.bridgelabz;
+import java.lang.Math;
+import java.util.Scanner;
+public class PrintMaxNumber {
 
-public class PrintMaxNumber
-{
 
-   public static void main(String[] args)
-   {
-	   System.out.println("welcome to test max of 3 number using generic");
-     }
- }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input 3 integers: ");
+        int x = Integer.parseInt(sc.nextLine());
+        int y = Integer.parseInt(sc.nextLine());
+        int z = Integer.parseInt(sc.nextLine());
+        int max = Math.max(x,y);
+        if(max>y)
+        {
+            max = Math.max(x,z);    
+        }
+        else{ 
+            max = Math.max(y,z);    
+        }
+        System.out.println("The max of three is: " + max);
+    }
+} 
