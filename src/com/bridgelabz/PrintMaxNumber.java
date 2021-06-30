4f00783c82f03cc9bb8a11774000107a49a16b48
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-30 23:11:41 +0530

    ADDED: refactored code using generic

--- src/com/bridgelabz/PrintMaxNumber.java
+++ src/com/bridgelabz/PrintMaxNumber.java
@@ -1,15 +1,21 @@
 package com.bridgelabz;
 
-public class PrintMaxNumber {
-
-
-   
-    	    Integer num1=1,num2=2,num3=3;
-	        Float Num1=1.11f,Num2=2.2f,Num3=3.3f;
-	        
-	    
-	    
-		    public static <T extends Comparable<T>> T maximum(T x, T y, T z) 
+public class PrintMaxNumber 
+	    {
+	   public static void main(String args[])
+	    {
+		   Integer num1=3,num2=3,num3=5;
+		   Float Num1=6.6f,Num2=7.2f,Num3=9.8f;
+           String firstString="apple",secondString="peach",thirdString="banana";
+		       
+		   System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+	       System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 7.2, 9.8, maximum(6.6,
+		                7.2, 9.8));
+		   System.out.printf("Maximum of %s, %s and %s is %s\n",  "apple", "peach","banana", maximum(
+		                 "apple", "peach","banana"));
+		 }
+	   
+		 public static <T extends Comparable<T>> T maximum(T x, T y, T z) 
 		    {
 		        T max = x;
 
@@ -21,10 +27,5 @@ public class PrintMaxNumber {
 
 		        return max;
 		    }
-
-		    public static void main(String args[]) {
-		        System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
-		        System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
-		                8.8, 7.7));
-		    }
 }
+
