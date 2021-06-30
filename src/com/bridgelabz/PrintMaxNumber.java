00ba3ecbc5b60db5d2c53175f6f250b7c72b63ab
shreyasalanke <shreyasalanke97@gmail.com>
2021-06-30 22:18:29 +0530

    ADDED: find test maximum

---
+++ src/com/bridgelabz/PrintMaxNumber.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class PrintMaxNumber
+{
+
+   public static void main(String[] args)
+   {
+	   System.out.println("welcome to test max of 3 number using generic");
+     }
+ }
